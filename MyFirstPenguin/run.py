c2361fb960272d4908c17784395ea45ff06bad30
Martin Falang <martfal@stud.ntnu.no>
2018-11-07 13:12:02 +0100

    added shootenemy

--- MyFirstPenguin/run.py
+++ MyFirstPenguin/run.py
@@ -68,8 +68,37 @@ def moveRandom(body):
 def chooseAction(body):
     action = PASS
     #action = moveTowardsCenterOfMap(body)
-    action = moveRandom(body)
+    if shootIfPossible(body):
+        action = SHOOT
+    else:
+        action = moveRandom(body)
     return action
+
+
+def shootIfPossible(body):
+    
+    you = body["you"]
+    direction = you["direction"]
+    myPosX = you["x"]
+    myPosY = you["y"]
+    enemy = body["enemies"][0]
+    try:
+        enX = enemy["x"]
+        enY = enemy["y"]
+    except:
+        return False
+
+    if direction == "right" and enX - myPosX > 0 and enY == myPosY:
+        return True
+    elif direction == "left" and enX - myPosX < 0 and enY == myPosY:
+        return True
+    elif direction == "bottom" and enY - myPosY > 0 and enX == myPosX:
+        return True
+    elif direction == "top" and enY - myPosY < 0 and enX == myPosX:
+        return True
+    else:
+        return False
+
 
 env = os.environ
 req_params_query = env['REQ_PARAMS_QUERY']
